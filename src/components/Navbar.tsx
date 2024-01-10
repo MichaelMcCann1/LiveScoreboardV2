b1b87933de92ab65d75b01719a7726a1787df8f1
Michael McCann <mmccann0903@gmail.com>
2024-01-10 16:32:26 -0600

    update navbar highlight

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
+const isActivePathname = (pathname: string, link: string) => {
+  if (pathname === "/" && link === "/") return true;
+  if (link === "/") return false;
+  return pathname.includes(link);
+};
+
 const linkData = [
   { text: "Home", href: "/" },
   { text: "NFL", href: "/nfl" },
@@ -19,9 +25,12 @@ export default function Navbar() {
         {linkData.map((link) => (
           <Link
             key={link.text}
-            className={classNames("text-xl font-medium ease-linear duration-200 hover:text-blue-600", {
-              underline: pathname === link.href,
-            })}
+            className={classNames(
+              "text-xl font-medium ease-linear duration-200 hover:text-blue-600",
+              {
+                underline: isActivePathname(pathname, link.href),
+              }
+            )}
             href={link.href}
           >
             {link.text}
