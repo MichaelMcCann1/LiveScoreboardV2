02ddc63a24426a4ced19ccd006e69b5402e2befd
Michael McCann <mmccann0903@gmail.com>
2024-01-10 16:28:58 -0600

    add player page content

--- src/data/queries.ts
+++ src/data/queries.ts
@@ -351,14 +351,85 @@ export const useNflTeamLeaders = (team: string) => {
   return finalData as UseQueryResult<NflTeamLeaderData[], Error>;
 };
 
+interface NflPlayerData {
+  firstName: string;
+  lastName: string;
+  jersey: string;
+  position: string;
+  height: string;
+  weight: string;
+  draft: string;
+  headshot: string;
+  teamLink: string;
+  location: string;
+  nickname: string;
+  logo: string;
+  abbreviation: string;
+  age: number;
+  city: string;
+  state: string;
+}
+
+const formatNflPlayerData = (data: any) => {
+  console.log(data)
+  return {
+    firstName: data.firstName,
+    lastName: data.lastName,
+    jersey: data.jersey,
+    position: data.position.displayName,
+    height: data.displayHeight,
+    weight: data.displayWeight,
+    draft: data.draft.displayText,
+    headshot: data.headshot.href,
+    teamLink: data.team.$ref,
+    age: data.age,
+    city: data.birthPlace.city,
+    state: data.birthPlace.state,
+  };
+};
+
+const formatNflPlayerTeamData = (data: any) => {
+  return {
+    location: data.location,
+    nickname: data.nickname,
+    logo: data.logos[0].href,
+    abbreviation: data.abbreviation,
+  };
+};
+
 export const useNflPlayer = (playerID: string) => {
-  return useQuery({
-    queryKey: ["nflPlayer", playerID],
+  const playerInfoQuery = useQuery({
+    queryKey: ["nflPlayerInfo", playerID],
     queryFn: async () => {
       return axiosHandler(
-        `https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/athletes/${playerID}/overview`
+        `http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2023/athletes/${playerID}`
       );
     },
-    select: (data) => data,
+    select: (data) => formatNflPlayerData(data),
   });
+
+  const playerTeamQuery = useQuery({
+    queryKey: ["nflPlayerTeam"],
+    queryFn: async () => {
+      return axiosHandler(playerInfoQuery.data?.teamLink);
+    },
+    enabled: !!playerInfoQuery.data?.teamLink,
+    select: (data) => formatNflPlayerTeamData(data),
+  });
+
+  const returnQuery = useMemo(() => {
+    if (!playerTeamQuery.data) {
+      return { ...playerInfoQuery, isLoading: true };
+    }
+
+    return {
+      ...playerInfoQuery,
+      data: {
+        ...playerInfoQuery.data,
+        ...playerTeamQuery.data,
+      },
+    };
+  }, [playerTeamQuery.data, playerInfoQuery]);
+
+  return returnQuery as UseQueryResult<NflPlayerData, Error>;
 };
